21a027354af865164736ea045836148337f3eda1
Daniel Martins Antunes <danoan2008@gmail.com>
2024-05-20 17:12:38 +0200

    Use gpt-4o

--- examples/convert-to-perchance-format/convert-to-perchance-format.sh
+++ examples/convert-to-perchance-format/convert-to-perchance-format.sh
@@ -7,5 +7,5 @@ OUTPUT_FOLDER="${SCRIPT_PATH}/output"
 mkdir -p "${OUTPUT_FOLDER}"
 
 pushd "${SCRIPT_FOLDER}" >/dev/null
-perchance-tools convert-to-perchance-format "${INPUT_FOLDER}/chunk.yml" >"${OUTPUT_FOLDER}/chunk.yml"
+perchance-tools convert-to-perchance-format "${INPUT_FOLDER}/chunk.yml" >"${OUTPUT_FOLDER}/chunk-perchance.yml"
 popd >/dev/null
